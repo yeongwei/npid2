2b9670abe9ec9f1a741c34558560022f1374a124
YeongWei <laiyw@persistent.my>
2016-02-05 03:58:08 -0500

    Corrected logic for checkAllStatus

--- docker/npi_env.sh
+++ docker/npi_env.sh
@@ -1,9 +1,10 @@
 # Sample template environment file for Docker, source this
 # in the user profile that start the Docker service.
 
+# Defaults to standalone environment file ???
 export HOSTNAME=$(hostname)
-export HADOOP_NAMENODE_URL=hdfs://npi-nn-spark-hadoop:9000/
-export ZOOKEEP_URL=npi-zk:2181
-export NPI_STORAGE_HOST=npi-storage
+export HADOOP_NAMENODE_URL=hdfs://${HOSTNAME}:9000/
+export ZOOKEEP_URL=${HOSTNAME}:2181
+export NPI_STORAGE_HOST=${HOSTNAME}
 export NPI_WORK_PATH=/data/work
 # export NPI_CONF_PATH=/data/npi-conf    # optional if overriding NPI default /opt/npi/conf
