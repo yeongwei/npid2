d15b2b95e6fefb2f167dcec2db7b4c20a37e5045
YeongWei <laiyw@persistent.my>
2016-02-19 01:02:14 -0500

    Consolidated environment variables

--- docker/npi_env.sh
+++ docker/npi_env.sh
@@ -8,3 +8,15 @@ export ZOOKEEP_URL=${HOSTNAME}:2181
 export NPI_STORAGE_HOST=${HOSTNAME}
 export NPI_WORK_PATH=/data/work
 # export NPI_CONF_PATH=/data/npi-conf    # optional if overriding NPI default /opt/npi/conf
+
+export NAMENODE=true
+export USER_CONFIG=false
+
+export HADOOP_PREFIX=${SERVICES_DIR}/hadoop
+export STORAGE_URL=$HADOOP_NAMENODE_URL
+export ZK_URL=$ZOOKEEP_URL
+
+export JDBC_SERVICE=${JDBC_SERVICE:-"`hostname`:8091"}
+export HADOOP_RM_PORT=${HADOOP_RM_PORT:-8088}
+export HADOOP_PID_DIR=${VAR_DIR:-"/tmp"}
+export YARN_PID_DIR=${VAR_DIR:-"/tmp"}
